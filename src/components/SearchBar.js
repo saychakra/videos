faf4269158cb46607b1913965f03a3e4565d1547
saychakra <saychakra@gmail.com>
2020-09-21 20:18:52 +0530

    made searchbar form controlled

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,25 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
+  // this is an event callback, hence binding problem may arise, hence, we need to assign it as an
+  // arrow function.
+  onInputChangerFunctionCustom = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
   render() {
     return (
       <div className="search-bar ui segment">
         <form className="ui form">
           <div className="field">
             <label>Video Search</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChangerFunctionCustom}
+            />
           </div>
         </form>
       </div>
